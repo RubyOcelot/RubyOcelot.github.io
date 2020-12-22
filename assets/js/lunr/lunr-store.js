2e4683d392c193d0e1d84e615b2ea73f4aa5d5c4
GitHub <noreply@github.com>
2020-12-22 02:52:25 +0000

    Auto deploy from GitHub Actions build 5

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First post",
+        "excerpt":"My First Post   Hi, there!     ","categories": [],
+        "tags": ["test"],
+        "url": "https://loliw.moe/first-post/",
+        "teaser": "https://loliw.moe/assets/images/teaser-default.jpg"
+      }]
