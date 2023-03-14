4b454c6298ab1281558d2764d80522c815d112b4
GitHub <noreply@github.com>
2023-03-14 03:56:16 +0000

    Auto deploy from GitHub Actions build 21

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "First post",
         "excerpt":"My First Post   Hi, there!     ","categories": [],
         "tags": ["test"],
-        "url": "https://loliw.moe/first-post/",
-        "teaser": "https://loliw.moe/assets/images/teaser-default.jpg"
+        "url": "/first-post/",
+        "teaser": "/assets/images/teaser-default.jpg"
       }]
